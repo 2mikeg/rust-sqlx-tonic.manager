8640750a4ca453af0b94f5075134334598558c4d
mike <michael.guzman.personal@gmail.com>
2023-12-12 20:34:11 -0500

    plain protobuf struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use cas::cas_server::{Cas, CasServer};
-use cas::{EsSetlement, EsSetlementBase, EsSettlementCreate, EsSettlementGet};
+use cas::{EsSetlement, EsSettlementCreate, EsSettlementGet};
 use tonic::{transport::Server, Request, Response, Status};
 
 pub mod cas {
@@ -15,18 +15,15 @@ impl Cas for NewCas {
         &self,
         request: Request<EsSettlementCreate>,
     ) -> Result<Response<EsSetlement>, Status> {
-        let req = request.into_inner();
-        let req_settlement = req.settlement.unwrap();
+        let req: EsSettlementCreate = request.into_inner();
 
         let resp = cas::EsSetlement {
             id: "asd-asd-asd".parse().unwrap(),
-            settlement: Some(EsSetlementBase {
-                service_id: req_settlement.service_id,
-                quantity: req_settlement.quantity,
-                price: req_settlement.quantity,
-                amount: req_settlement.amount,
-            }),
+            service_id: req.service_id,
             created_at: "2023-01-01".parse().unwrap(),
+            quantity: req.quantity,
+            price: req.price,
+            amount: req.amount,
         };
 
         Ok(Response::new(resp))
@@ -39,12 +36,10 @@ impl Cas for NewCas {
         let req = request.into_inner();
         let resp = EsSetlement {
             id: req.id,
-            settlement: Some(EsSetlementBase {
-                service_id: "asd-asd-asd".parse().unwrap(),
-                quantity: 1.0,
-                price: 1.0,
-                amount: 1.0,
-            }),
+            service_id: "asd-asd-asd".parse().unwrap(),
+            quantity: 1.0,
+            price: 1.0,
+            amount: 1.0,
             created_at: "2023-01-01".parse().unwrap(),
         };
 
